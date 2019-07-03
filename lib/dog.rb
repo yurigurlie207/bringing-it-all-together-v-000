904c3015922cad624c68bed2e3fb9d6de85e51ac
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-03 16:12:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,7 +9,14 @@ class Dog
   end
 
   def self.create_table
-    
+    sql =  <<-SQL
+     CREATE TABLE IF NOT EXISTS dogs (
+       id INTEGER PRIMARY KEY,
+       name TEXT,
+       breed TEXT
+       )
+   SQL
+   DB[:conn].execute(sql)
   end
 
 

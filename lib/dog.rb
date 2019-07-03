18c50f519ab5373aa9429596ffe01fefd53e3c40
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-03 16:18:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,5 +48,17 @@ class Dog
     dog.save
     dog
   end
-  
+
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql,id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end

3c70bdd53a6f627070f06b93865392d309146712
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-03 16:21:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -78,15 +78,16 @@ class Dog
          LIMIT 1
     SQL
 
-   dog = DB[:conn].execute(sql,name,breed)
+    dog = DB[:conn].execute(sql,name,breed)
 
-   if !dog.empty?
-     dog_data = dog[0]
-     dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
-   else
-     dog = self.create(name: name, breed: breed)
+     if !dog.empty?
+       dog_data = dog[0]
+       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+     else
+       dog = self.create(name: name, breed: breed)
+     end
+
+     dog
    end
-   dog
- end
 
 end
